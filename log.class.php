402060bb0375586248189dc7bda3b279d50ed712
Bahadır AKKÖY <bahadir.akkoy@gmail.com>
2021-11-02 15:53:44 +0300

    Update log.class.php

--- log.class.php
+++ log.class.php
@@ -9,9 +9,7 @@
 			
 		    # @string, Log directory name
 		    	private $path = 'logs/';
-			
-			
-			
+
 		    # @void, Default Constructor, Sets the timezone and path of the log files.
 			public function __construct() {
 				date_default_timezone_set('Europe/Istanbul');	
